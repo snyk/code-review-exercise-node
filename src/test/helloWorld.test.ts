95291868b821bc10b6addea8d6a316be01eab541
Nico Carl <nico.carl@snyk.io>
2025-01-16 13:14:48 +0100

    feat: remove hello world boilerplate

--- src/test/helloWorld.test.ts
+++
@@ -1,7 +0,0 @@
-import { sayHello } from "../service/helloWorld";
-
-test("sayHello with name returns hello name", () => {
-  const result = sayHello("World");
-
-  expect(result).toEqual("Hello World");
-});
